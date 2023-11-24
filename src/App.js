00598ac941b3ad62600d4dde811d1a8090fabb08
nijataliyev1 <nicat3699@gmail.com>
2023-11-24 15:48:54 +0400

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+
 
 function App() {
-  return (
+  const [data,setData] = useState([])
+  const [searchName, setSearchName] = useState("")
+  const [searchDepartment, setSearchDepartment] = useState("")
+  const searchNameFunc = (e) => {
+    setSearchName(e.target.value.toLowerCase())
+  }
+  const searchDepartmentFunc = (e) => {
+    setSearchDepartment(e.target.value.toLowerCase())
+  }
+  useEffect(() => {
+    fetch("https://5ea5ca472d86f00016b4626d.mockapi.io/brotherhood")
+    .then(res => res.json())
+    .then(data => setData(data))
+  },[])
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type = "text" className='name-search' placeholder='Name' onChange={searchNameFunc}/>
+      <input type = "text" className='department-search' placeholder='Department' onChange={searchDepartmentFunc}/>
+      <div className='cards'>
+      {
+        data.filter((i) => (i.name.toLowerCase().includes(searchName) && i.department.toLowerCase().includes(searchDepartment))).map((item) => (
+          <div className='card' key = {item.id}>
+              <p className='name'>Name: {item.name}</p>
+              <p className='department'>Department: {item.department}</p>
+              <p className='role'>Role: {item.role}</p>
+          </div>
+        ))
+        
+      }
+      </div>
     </div>
   );
 }
